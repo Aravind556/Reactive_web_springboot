3fc32f45f1d9fff42cb6f6ef9af8350ae3374a3c
Aravind556 <chintalapatiaravind30@gmail.com>
2025-06-17 20:35:29 +0100

    daily pushing

--- src/main/java/com/example/Demo/Reactive.java
+++ src/main/java/com/example/Demo/Reactive.java
@@ -1,6 +1,6 @@
 package com.example.Demo;
 
-
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Reactive {
@@ -9,10 +9,16 @@ public class Reactive {
         return Mono.just("Hello :)");
     }
 
+    private Flux<String> testflux(){
+        return Flux.just("Hello","HI","wassup");
+    }
+
     public static void main(String [] args){
 
         Reactive react = new Reactive();
         react.testMono()
+                .subscribe(data -> System.out.println(data));
+        react.testflux()
                 .subscribe(data -> System.out.println(data));
 
 }
